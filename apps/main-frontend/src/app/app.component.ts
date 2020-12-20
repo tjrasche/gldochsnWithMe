cda5c93509de4aeb8f2874793abf44e42b3dd94d
tjark <t.rasche@rrooaarr.com>
2020-12-20 16:56:36 +0100

    angular-app generated

---
+++ apps/main-frontend/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'go-list-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'main-frontend';
+}
